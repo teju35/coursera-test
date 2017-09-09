d5859c38b869a085ab99a6667067a5e6d41dc3a1
teju35 <30970880+teju35@users.noreply.github.com>
2017-09-09 14:17:56 +0530

    Update script.js

--- project4/js/script.js
+++ project4/js/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Emma", "John", "Jen", "James", "Harry", "Ron", "Lily", "Jerry", "Luna", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
